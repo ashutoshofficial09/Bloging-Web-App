cec0c7c8d1403dd8877335ed86988d62ecc6b80c
ashutoshofficial09 <ashutoshm9695@gmail.com>
2022-01-08 12:46:59 +0530

    ..

--- frontend/src/components/Manage Blogs/manageBlogs.js
+++ frontend/src/components/Manage Blogs/manageBlogs.js
@@ -1,22 +1,29 @@
+import { Alert, Button, Snackbar } from "@mui/material";
+import MDEditor from "@uiw/react-md-editor";
 import { useState } from "react";
 import appConfig from "../../config.js";
 
-const ManageBlog = () => {
+const ManageBlogs = () => {
   const url = appConfig.api_url;
 
   const [blogList, setBlogList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [blogDetail, setBlogDetail] = useState([]);
 
   // to control snakbar
   const [snakbarOpen, setSnakbarOpen] = useState(false);
 
   //fetch data form backend
-  const fetchUsersData = () => {
-    fetch(url + "/blog/getall")
+  const fetchBlogData = () => {
+    fetch(
+      url +
+        "/blog/getbyauthor/" +
+        JSON.parse(sessionStorage.getItem("user"))._id
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
-        setBlogList(data);
+        setBlogDetail(data);
         setLoading(false);
       });
   };
@@ -30,7 +37,7 @@ const ManageBlog = () => {
         if (res.status === 200) {
           console.log("item deleted");
           setSnakbarOpen(true);
-          fetchUsersData();
+          fetchBlogData();
           return res.json();
         }
       })
@@ -39,7 +46,65 @@ const ManageBlog = () => {
       });
   };
 
-  return;
-  <div></div>;
+  const showBlogs = () => {
+    if (!loading) {
+      return blogList.map((blog, index) => (
+        <tr key={index}>
+          <td>{blog.thumbnail}</td>
+          <td>{blog.title}</td>
+          <td>{blog.description}</td>
+          <td>
+            <Button
+              onClick={() => {
+                deleteBlog(blog._id);
+              }}
+              variant="contained"
+              color="error"
+            >
+              <i className="fas fa-trash-alt"></i>
+              &nbsp; Delete
+            </Button>
+          </td>
+        </tr>
+      ));
+    } else {
+      return <h2>Loading ...</h2>;
+    }
+  };
+  const closeSnackBar = () => {
+    setSnakbarOpen(false);
+  };
+
+  return (
+    <div className="container">
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        open={snakbarOpen}
+        onClose={closeSnackBar}
+      >
+        <Alert
+          onClose={closeSnackBar}
+          severity="success"
+          sx={{ width: "100%" }}
+        >
+          Blog Deleted Successfully!!
+        </Alert>
+      </Snackbar>
+      <h1>Manage Blogs</h1>
+      <hr />
+
+      <table className="table table-dark">
+        <thead>
+          <tr>
+            <th>Thumbnail</th>
+            <th>Blog Title</th>
+            <th>Descriprion</th>
+            <th>Delete Blogs</th>
+          </tr>
+        </thead>
+        <tbody>{showBlogs()}</tbody>
+      </table>
+    </div>
+  );
 };
-export default ManageBlog;
+export default ManageBlogs;
